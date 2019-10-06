6d54c24dff0af36144a0b050bdceb1af597466eb
Will Lacey <WillLacey@Will-Laceys-MacBook-Pro-4.local>
2019-10-06 13:23:40 -0500

    Deleted DSPGraphModel, added AnalyzerModel

---
+++ Lab2/Project/AnalyzerModel.h
@@ -0,0 +1,17 @@
+//
+//  AnalyzerModel.h
+//  Lab2
+//
+//  Created by Will Lacey on 10/6/19.
+//  Copyright © 2019 Will Lacey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AnalyzerModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Lab2/Project/AnalyzerModel.m
@@ -0,0 +1,13 @@
+//
+//  AnalyzerModel.m
+//  Lab2
+//
+//  Created by Will Lacey on 10/6/19.
+//  Copyright © 2019 Will Lacey. All rights reserved.
+//
+
+#import "AnalyzerModel.h"
+
+@implementation AnalyzerModel
+
+@end
